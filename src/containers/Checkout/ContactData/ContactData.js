948570dcf72a2a05dedbcde357ce34a0e43996b1
Praval Jain <praval41195jain@gmail.com>
2018-09-25 10:54:15 +0530

    upto post form data [Before Redux]

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
- 
+
 import classes from './ContactData.css';
 import axios from '../../../axios-order';
 import Button from '../../../components/UI/Button/Button';
@@ -65,19 +65,25 @@ class ContactData extends Component {
 
         orderHandler = (event)=> {
             event.preventDefault();
-                this.setState({loading : true});
-        const order ={
+            this.setState({loading : true});
+            const formData = {}
+            for(let formElementIdentifier in this.state.orderForm) {
+                formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+            }
+
+            const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
+            orderData : formData
         }
-    axios.post('/orders.json', order)
-    .then(response => {
-        this.setState({ 
+        axios.post('/orders.json', order)
+            .then(response => {
+            this.setState({ 
             loading: false , 
             purchasing: false,});
             this.props.history.push('/');
-    })
-    .catch(error => {
+        })
+        .catch(error => {
         this.setState({ loading: false, purchasing: false });
         });
         }
@@ -103,7 +109,7 @@ class ContactData extends Component {
                     });
                 }
                 let form = (
-                    <form>
+                    <form onSubmit={this.orderHandler}>
                     {formElementsArray.map((formElement) => {
                         return(
                         <Input 
@@ -113,7 +119,7 @@ class ContactData extends Component {
                         value={formElement.config.value} 
                         changed={(event) => this.inputChangedhandler(event, formElement.id)} />)
                     })}
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                    <Button btnType="Success">ORDER</Button>
                 </form>
                 );
                 if(this.state.loading) {
@@ -127,6 +133,6 @@ class ContactData extends Component {
                 );
             }
         
- }
+}
 
- export default ContactData;
+export default ContactData;
