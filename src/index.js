737df4c48cd19761d6cfde8519729a6a7278f74d
AzureAD\PravalJain <praval@cloudthing.com>
2019-02-07 10:21:29 +0530

    add persistance

--- src/index.js
+++ src/index.js
@@ -13,7 +13,17 @@ import orderReducer from './store/reducers/order';
 import fetchOrderReducer from './store/reducers/fetchOrders';
 import resetStatesReducer from './store/reducers/resetStates';
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
 
 const appReducer = combineReducers({
     burgerBuilder:reducer,
@@ -29,13 +39,20 @@ const rootReducer = (state , action ) => {
     }
         return appReducer(state, action)
 }
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+
+const  persistor = persistStore(store)
 
 const app = (
     <Provider store = {store}>
+     <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
+        </PersistGate>
     </Provider>
 ); 
 
